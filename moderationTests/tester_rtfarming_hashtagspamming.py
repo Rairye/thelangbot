edce65fbecc66edf3bbaddddeb70b6e0b8c9538c
Rairye <37942271+Rairye@users.noreply.github.com>
2022-02-27 06:40:27 +0900

    Add files via upload

---
+++ moderationTests/tester_rtfarming_hashtagspamming.py
@@ -0,0 +1,252 @@
+# Tester for checking for possible attempts to farm retweets.
+# Also checks for blacklisted hash tags and tweets with an excessive number of hashtags.
+
+import os, importlib, time
+from mocks import mock_t
+import traceback
+
+# ht-getter can be installed using pip.
+from ht_getter.getter import get_hash_tags
+
+mydb = None
+mycursor = None
+
+#Sets a threshold for an upper limit of tweets within a certain time period.
+#Users who exceed this threshold are ignored and none of their tweets within that time period are retweeted.
+tweetthreshold = 5
+
+#Checks to see if the mysql module is installed and, when it is, the database and cursor are initialized. 
+def setupDb() -> None:
+    mysql = None
+    try:
+        mysql = importlib.import_module("mysql.connector")
+    except:
+        print(traceback.format_exc(), flush=True)
+        return
+        
+    global mydb, mycursor
+    
+    try:
+        mydb = mysql.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            database=os.getenv("DB_DB"))
+        mycursor = mydb.cursor()
+    except:
+        print(traceback.format_exc(), flush=True)
+        
+
+def getBlacklist() -> set:
+    if mycursor == None:
+        return set([])
+
+    try:
+        mycursor.execute(
+            "SELECT * FROM blacklist")
+        myresult = mycursor.fetchall()
+        usernames = set([row[0] for row in myresult])
+        return usernames
+    except:
+        print(traceback.format_exc(), flush=True)
+        return set([])
+        
+def getSupporters() -> set:
+    if mycursor == None:
+        return set([])
+
+    try:
+        mycursor.execute(
+            "SELECT * FROM supporter")
+        myresult = mycursor.fetchall()
+        usernames = set([row[0] for row in myresult])
+        return usernames
+    except:
+        print(traceback.format_exc(), flush=True)
+        return set([])
+        
+def retrieveLastSeenId() -> int:
+    if mycursor == None:
+        return 0
+
+    try:
+        mycursor.execute("SELECT * FROM tweet")
+        myresult = mycursor.fetchall()
+        return myresult[0][1]
+    except:
+        print(traceback.format_exc(), flush=True)
+        return 0
+
+
+def storeLastSeenId(lastSeenId: int) -> None:
+    if mycursor == None:
+        return
+
+    try:
+        exampleId: int = (lastSeenId)
+        mycursor.execute("UPDATE tweet SET tweetId = '%s' WHERE id = 1", (exampleId,))
+        mydb.commit()
+        print(mycursor.rowcount, "record(s) affected", flush=True)
+
+    except:
+        print(traceback.format_exc(), flush=True)
+
+    return
+
+def getUserTweetMap(tweets : [], blackList : set([])) -> {}:
+    userTweetMap = {}
+    
+    for tweet in tweets:
+        twitterUser: str = tweet.user.screen_name
+        if twitterUser in blackList:
+            continue
+        
+        userTweets = userTweetMap.get(twitterUser, [])
+        if len(userTweets) == 0:
+            userTweetMap[twitterUser] = [tweet]
+        else:
+            userTweets.append(tweet)
+
+    return userTweetMap
+
+#Method for determining if a tweet is valid and if it should be retweeted/liked.
+def isValidTweet(tweet : str) -> bool:
+    #Upper limit of hashtags in a tweet.
+    maxHashTags = 10
+    #Should all be in lowercase.
+    blackListedHashTags = set(["#apples"])
+    
+    hashTags = [hashTag.lower() for hashTag in get_hash_tags(tweet)]
+
+    if len(hashTags) > maxHashTags:
+        return False
+
+    for hashTag in hashTags:
+        if hashTag in blackListedHashTags:
+            return False
+        
+    return True
+
+
+#By default, databases are not updated.
+def main(tweets : list, updateDB = False) -> None:
+    # Obtain last seen tweet
+    lastSeenId: int = retrieveLastSeenId()
+    print("Last seen tweet: " + str(lastSeenId) + "\n", flush=True)
+
+    # Setup current last seen tweet to be the previous one
+    # This is just in case there are no items in the iterator
+    currLastSeenId: int = lastSeenId
+
+    # Get blacklist here
+    blackList : set = getBlacklist()
+        
+    # Get supporters here
+    supporters : set = getSupporters()
+
+    userTweetMap : {} = getUserTweetMap(tweets, blackList)
+
+    for twitterUser in  userTweetMap:
+        tweets : [] =  userTweetMap[twitterUser]
+        tweetCount = len(tweets)
+
+        if tweetCount  > tweetthreshold:
+            continue
+
+        for i in range(min(2, tweetCount)):
+            tweet = tweets[i]
+
+            try:
+
+                if isValidTweet(tweet.text):
+                    
+                    # Like tweet if supporter
+                    if twitterUser in supporters:
+                        tweet.favorite()
+                        print("Liking tweet by" + twitterUser, flush=True)
+
+                    # Retweet post
+                    print("Retweet Bot found tweet by @" + 
+                        twitterUser + ". " + "Attempting to retweet...", flush=True)
+                    tweet.retweet()
+                    print(tweet.text, flush=True)
+                    print("Tweet retweeted!", flush=True)
+
+                    # Update last seen tweet with the newest tweet (bottom of list)
+                    currLastSeenId = tweet.id
+                    time.sleep(5)
+
+            except StopIteration:
+                print("Stopping...", flush=True)
+                break
+            
+            except Exception:
+                print(traceback.format_exc(), "Tweet id: " + str(tweet.id), flush=True)
+
+
+    # After iteration, store the last seen tweet id (newest)
+    # Only store if it is different and updateDB == True
+    if(updateDB == True and lastSeenId != currLastSeenId):
+        storeLastSeenId(currLastSeenId)
+        print("Updating last seen tweet to: " +
+        str(currLastSeenId) + "\n", flush=True)
+
+    return
+
+
+if __name__ == "__main__":
+    setupDb()
+
+    #There are 6 tweets from the same user. Since tweetthreshold = 5, none of those tweets will be retweeted. 
+
+    print("\nThere are 6 tweets from the same user. Since tweetthreshold = 5, none of those tweets will be retweeted. ", flush=True)
+
+    mock_t_list = [mock_t("user", "I study English."),
+                mock_t("user", "I study Japanese."),
+              mock_t("user", "I study Korean."),
+              mock_t("user", "I study English."),
+                mock_t("user", "I study Japanese."),
+                mock_t("user", "I study English."),
+
+              ]
+
+    print("\nTest 1 complete.", flush=True)
+
+    
+    main(mock_t_list)
+
+    #There are 5 tweets from the same user. Since tweetthreshold = 5, only the first 2 tweets will be retweeted. 
+
+    print("\nThere are 5 tweets from the same user. Since tweetthreshold = 5, only the first 2 tweets will be retweeted.", flush=True)
+    
+    mock_t_list = [mock_t("user", "I study English."),
+                mock_t("user", "I study Japanese."),
+              mock_t("user", "I study Korean."),
+              mock_t("user", "I study English."),
+                mock_t("user", "I study Japanese."),
+
+              ]
+    
+    main(mock_t_list)
+
+    print("\nTest 2 complete.", flush=True)
+
+
+    print("\nFirst tweet contains a valid hashtag. Second tweet contains a blacklisted hashtag. Third tweet contains too many hashtags.", flush=True)
+
+    mock_t_list = [mock_t("user", "I study English. #langtwt"),
+                    mock_t("user", "I study Korean. #langtwt #apples"),
+                    mock_t("User", "#a #b #c #d #e #f #g #h #i #j #k #l #m #n #o #p"),
+              ]
+    
+    main(mock_t_list)
+
+    print("\nTest 3 complete.", flush=True)
+
+    
+    if mycursor != None:
+        mycursor.close()
+    if mydb != None:
+        mydb.close()
+        
+    print("\nRetweet function completed and db connection closed", flush=True)
